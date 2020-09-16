6900a0be3d0fc9a858e61b869699cebc6038e37c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-09-16 16:22:53 -0400

    pre-render post data using static generation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { getRedirectStatus } from "next/dist/lib/check-custom-routes";
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,11 +12,30 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm Jesse and I'm learning how to use Next.js!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
